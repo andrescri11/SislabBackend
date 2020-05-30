5397731e14c59f336de7ef842390c85e0cdc693e
DESKTOP-2PCUVK0\molin <molinaraul5219@gmail.com>
2020-05-30 18:28:47 -0500

    Creacion constantes

---
+++ FrontEndSislab/SislabFrontEnd/src/app/_shared/var.constant.ts
@@ -0,0 +1,12 @@
+// export const HOST = 'http://localhost:8081/';
+// export const HOST2 = 'http://localhost:8082/';
+// export const HOST3 = 'http://localhost:8083/';
+export const HOST = 'http://localhost:8099/micro1/';
+export const HOST2 = 'http://localhost:8099/micro2/';
+export const HOST3 = 'http://localhost:8099/micro3/';
+export const HOSTG = 'http://localhost:8099/uaa/';
+export const facturacion = 'http://localhost:8084/factura';
+export const TOKEN_AUTH_USERNAME = 'microserv2';
+export const TOKEN_AUTH_PASSWORD = 'sislab19codex';
+export const TOKEN_NAME = 'access_token';
+export const REINTENTOS = 3;
