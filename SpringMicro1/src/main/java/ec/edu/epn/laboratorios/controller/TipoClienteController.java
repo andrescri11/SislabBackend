9c05ac6fe12f59a9e5b77bd76c987deda82fc258
DESKTOP-2PCUVK0\molin <molinaraul5219@gmail.com>
2020-05-30 12:25:11 -0500

    Tipo cliente

---
+++ SpringMicro1/src/main/java/ec/edu/epn/laboratorios/controller/TipoClienteController.java
@@ -0,0 +1,55 @@
+package ec.edu.epn.laboratorios.contoller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ec.edu.epn.laboratorios.model.TipoCliente;
+import ec.edu.epn.laboratorios.service.TipoClienteService;
+
+@RestController
+@RequestMapping("/tipoClientes")
+public class TipoClienteController {
+	
+	@Autowired
+	private TipoClienteService service;
+	
+	@PreAuthorize("hasAuthority('ADMIN')")
+	@GetMapping
+	public List<TipoCliente> getTcliente() {
+		return service.getTcliente();
+	}
+	
+	@PreAuthorize("hasAuthority('ADMIN')")
+	@GetMapping(value = "/{id}")
+	public TipoCliente consultarTipoCliente(@PathVariable("id") String id) {
+		return service.consultarTipoCliente(id);
+	}
+
+	@PreAuthorize("hasAuthority('ADMIN')")
+	@PostMapping(consumes = "application/json", produces = "application/json")
+	public TipoCliente ingresarTipoCliente(@RequestBody TipoCliente tipoCliente) {
+		return service.ingresarTipoCliente(tipoCliente);
+	}
+
+	@PreAuthorize("hasAuthority('ADMIN')")
+	@PutMapping(consumes = "application/json", produces = "application/json")
+	public TipoCliente actualizarTipoCliente(@RequestBody TipoCliente tipoCliente) {
+		return service.actualizarTipoCliente(tipoCliente);
+	}
+
+	@PreAuthorize("hasAuthority('ADMIN')")
+	@DeleteMapping(value = "/{id}")
+	public void eliminarTipoCliente(@PathVariable("id") String id) {
+		service.eliminarTipoCliente(id);
+	}
+}
