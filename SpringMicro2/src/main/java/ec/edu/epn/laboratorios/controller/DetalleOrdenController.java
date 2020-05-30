9da79c2a5c181ce82baf0b2d60d978f367ccdf4b
DESKTOP-2PCUVK0\molin <molinaraul5219@gmail.com>
2020-05-30 13:22:23 -0500

    Detalle orden controlador

---
+++ SpringMicro2/src/main/java/ec/edu/epn/laboratorios/controller/DetalleOrdenController.java
@@ -0,0 +1,29 @@
+package ec.edu.epn.laboratorios.contoller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ec.edu.epn.laboratorios.model.DetalleOrden;
+import ec.edu.epn.laboratorios.service.DetalleOrdenService;
+
+@RestController
+@RequestMapping("/DetalleOrden")
+public class DetalleOrdenController {
+	
+	@Autowired
+	private DetalleOrdenService service;
+
+	@GetMapping
+	public List<DetalleOrden> getListaDetalle() {
+		return service.getListaDetalle();
+	}
+}
