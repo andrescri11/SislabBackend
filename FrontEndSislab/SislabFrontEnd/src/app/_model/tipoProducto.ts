888750723dbe41b2aab65f3ed9b1ceb1a4c88c0c
DESKTOP-2PCUVK0\molin <molinaraul5219@gmail.com>
2020-05-30 18:15:34 -0500

    Pruebas ms1

---
+++ FrontEndSislab/SislabFrontEnd/src/app/_model/tipoProducto.ts
@@ -0,0 +1,8 @@
+export class TipoProducto {
+    // tslint:disable-next-line:variable-name
+    id_tipoprod: string;
+    // tslint:disable-next-line:variable-name
+    nombre_tprod: string;
+    // tslint:disable-next-line:variable-name
+    descr_tprod: string;
+}
---
+++ FrontEndSislab/SislabFrontEnd/src/app/_model/tipoProveedor.ts
@@ -0,0 +1,8 @@
+export class TipoProveedor {
+    // tslint:disable-next-line:variable-name
+    id_tipoproveedor: string;
+    // tslint:disable-next-line:variable-name
+    nombre_tpv: string;
+    // tslint:disable-next-line:variable-name
+    descr_tpv: string;
+}
