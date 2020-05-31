8a7409cbd2c0afde4052ed14152c18d9d81e75cb
DESKTOP-2PCUVK0\molin <molinaraul5219@gmail.com>
2020-05-30 19:36:14 -0500

    Dependencias ordenes de trabajo

--- SpringMicro2/src/main/java/ec/edu/epn/laboratorios/dao/PersonalDAO.java
+++ SpringMicro2/src/main/java/ec/edu/epn/laboratorios/dao/PersonalDAO.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.Query;
 import ec.edu.epn.laboratorios.model.Personal;
 
 public interface PersonalDAO extends JpaRepository<Personal, String>{
+
 	@Query(value="select MAX(aux_idpersonal) from personal", nativeQuery = true)
 	List<Object[]> auxiliarCodigoMaxProforma();
 	@Query(value="select nextval('secuencia_personal');", nativeQuery = true)
